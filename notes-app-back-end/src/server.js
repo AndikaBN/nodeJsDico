2fe4a68e1954a78b9a5399843ad37caab5497b86
AndikaBN <bintangnursalih275@gmail.com>
2024-06-06 11:21:49 +0800

    crud web server

---
+++ notes-app-back-end/src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+
+init().catch(err => {
+  console.error('Error starting server:', err);
+});
